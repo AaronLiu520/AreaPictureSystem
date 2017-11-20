1fe0f1120214c8958fe5fe6d5e592aa15d5c450e
G631233828 <631233828@qq.com>
2017-11-20 15:18:18 +0800

    添加app-FileSystem

---
+++ AreaPictureSystem/app-FileSystem-pojo/src/main/java/org/app/FileSystem/pojo/Source.java
@@ -0,0 +1,22 @@
+/**   
+* @Title: Source.java 
+* @Package org.app.FileSystem.pojo 
+* @Description: TODO(用一句话描述该文件做什么) 
+* @author fliay
+* @date 2017年11月20日 下午2:37:36 
+* @version V1.0   
+*/
+package org.app.FileSystem.pojo;
+
+import org.app.framework.pojo.GeneralBean;
+
+/** 
+* @ClassName: Source 
+* @Description: TODO(这里用一句话描述这个类的作用) 
+* @author fliay
+* @date 2017年11月20日 下午2:37:36 
+*  
+*/
+public class Source extends GeneralBean {
+
+}
