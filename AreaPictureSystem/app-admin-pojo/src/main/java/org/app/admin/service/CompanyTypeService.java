4dff8ddd02afdbbcb417901a03480e5b3ef0e5ce
ch <910692871@qq.com>
2017-11-24 13:33:31 +0800

    compangytype相关

---
+++ AreaPictureSystem/app-admin-pojo/src/main/java/org/app/admin/service/CompanyTypeService.java
@@ -0,0 +1,9 @@
+package org.app.admin.service;
+
+import org.app.admin.pojo.CompanyType;
+import org.app.framework.service.GeneralServiceImpl;
+import org.springframework.stereotype.Repository;
+@Repository("companyTypeService")
+public class CompanyTypeService extends GeneralServiceImpl<CompanyType>{
+
+}
